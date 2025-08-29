459952387cbba012100369ff2dc618c2ef990efd
Johnny Dickson <johnnydicksoncode@gmail.com>
2025-08-29 15:10:17 -0500

    dev(setup): configure networking resources

--- main.tf
+++ main.tf
@@ -3,62 +3,98 @@
 # for learning purposes.
 
 terraform {
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.0"
+      source  = "hashicorp/aws"
+      version = "6.11.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region    = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami             = "ami-011899242bb902164" # Ubunutu 20.04 LTS //us-east 1
+# EC2 Instance Setup
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name    = "name"
+    values  = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name    = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name    = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "learninfra-1" {
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = "t3.micro"
-}
+  user_data       = <<-EOF
+                    #!/bin/bash
+                    echo "Hello from Instance 1" > index.html
+                    python3 -m http.server 8080 &
+                    EOF
 
-## Remote Backend Bootstraping: Part 1 - local apply w/ requested resources.
-
-# Versioned & encrypted S3 bucket
-resource "aws_s3_bucket" "terraform_state" {
-  bucket          = "learn-iac-tf-state"
-  force_destroy   = true
-}
-
-resource "aws_s3_bucket_acl" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning_terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id 
-
-  versioning_configuration {
-    status = "Enabled"
+  tags = {
+    Name = "LearnInfra"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.arn
+resource "aws_instance" "learninfra-2" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  user_data       = <<-EOF
+                    #!/bin/bash
+                    echo "Hello from Instance 1" > index.html
+                    python3 -m http.server 8080 &
+                    EOF
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-  
-# DynamoDB table used for terraform locking
-resource "aws_dynamodb_table" "terraform_lock" {
-  name          = "terraform-state-locking"
-  billing_mode  = "PAY_PER_REQUEST"
-  hash_key      = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
+  tags = {
+    Name = "LearnInfra"
   }
 }
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Default subnet for us-east-1"
+  }
+}
+
+resource "aws_default_vpc" "default_az1" {
+  tags = {
+    Name = "Default VPC for us-east-1"
+  }
+}
+
+resource "aws_security_group" "instances" {
+  name    = "instance-security-group"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_inbound" {
+  security_group_id     = aws_security_group.instances.id
+  cidr_ipv4             = aws_default_vpc.default_az1.cidr_block
+  from_port             = 8080
+  ip_protocol           = "tcp"
+  to_port               = 8080
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id     = aws_security_group.instances.id
+  cidr_ipv4             = "0.0.0.0/0"
+  ip_protocol           = "-1" # all ports
+}
